21cfb53e097da04669e9702619b2213abaa708b7
hananeoba <hananeouldbabaali@gmail.com>
2024-10-25 16:20:39 +0100

    testing reomote data src impl throwing exception

--- lib/src/auth/data/data_source/auth_remote_data_source.dart
+++ lib/src/auth/data/data_source/auth_remote_data_source.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:project1/core/errors/exceptions.dart';
+import 'package:project1/core/utils/constants.dart';
 import 'package:project1/src/auth/data/models/user_model.dart';
 import "package:http/http.dart" as http;
 
@@ -26,7 +28,7 @@ class AuthRemoteDataSrcImpl implements AuthRemoteDataSource {
       {required String createdAt,
       required String avatar,
       required String name}) async {
-    final response = await _client.post(Uri.parse("$kGetUsers$kCreateUser"),
+    final response = await _client.post(Uri.parse("$kBaseUrl$kCreateUser"),
         body: jsonEncode(
           {
             "createdAt": createdAt,
@@ -34,6 +36,10 @@ class AuthRemoteDataSrcImpl implements AuthRemoteDataSource {
             "name": name,
           },
         ));
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      throw ApiException(
+          message: response.body, statusCode: response.statusCode);
+    }
   }
 
   @override
@@ -44,8 +50,9 @@ class AuthRemoteDataSrcImpl implements AuthRemoteDataSource {
 
   @override
   Future<List<UserModel>> getUsers() async {
-    // TODO: implement getUsers
-    throw UnimplementedError();
+    final Users = await _client.get(Uri.parse("$kBaseUrl$kCreateUser"))
+        as List<UserModel>;
+    return Users;
   }
 
   @override
--- test/lib/src/auth/data/authentication_remote_data_source_implementation_test.dart
+++ test/lib/src/auth/data/authentication_remote_data_source_implementation_test.dart
@@ -3,14 +3,17 @@ import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
+import 'package:project1/core/errors/exceptions.dart';
 import 'package:project1/core/utils/constants.dart';
 import 'package:project1/src/auth/data/data_source/auth_remote_data_source.dart';
+import 'package:project1/src/auth/data/repositories/authentication_repository_implementation.dart';
+import 'package:project1/src/auth/domain/repositories/authentication_repository.dart';
 
 class MockClient extends Mock implements http.Client {}
 
 void main() {
   late http.Client client;
-  late AuthRemoteDataSource authRemoteDataSource;
+  late AuthRemoteDataSrcImpl authRemoteDataSource;
 
   setUp(() {
     client = MockClient();
@@ -21,8 +24,8 @@ void main() {
   group("create user", () {
     test("should complete successfully when called the status is 200/201 ",
         () async {
-      when(() => client.post(any(), body: any(named: ""))).thenAnswer(
-          (_) async => http.Response('user creqteed successfully ', 201));
+      when(() => client.post(any(), body: any(named: "body"))).thenAnswer(
+          (_) async => http.Response('user created successfully ', 201));
       final methodCall = authRemoteDataSource.createUser;
       expect(methodCall(createdAt: "2024/10/15", avatar: "", name: "hanane"),
           completes);
@@ -31,6 +34,30 @@ void main() {
               body: jsonEncode(
                   {"createdAt": "2024/10/15", "avatar": "", "name": "hanane"})))
           .called(1);
+      verifyNoMoreInteractions(client);
+    });
+    test("should throw api exception when status code is not  201 or 200",
+        () async {
+      when(() => client.post(any(), body: any(named: "body")))
+          .thenAnswer((_) async => http.Response(
+                'invalid email address',
+                400,
+              ));
+      final methodCall = authRemoteDataSource.createUser;
+      expect(
+          () async =>
+              methodCall(createdAt: "2024/10/15", avatar: "", name: "hanane"),
+          throwsA(const ApiException(
+            message: "invalid email address",
+            statusCode: 400,
+          )));
+
+      verify(() => client.post(
+            Uri.parse('${kBaseUrl}${kCreateUser}'),
+            body: jsonEncode(
+              {"createdAt": "2024/10/15", "avatar": "", "name": "hanane"},
+            ),
+          )).called(1);
       verifyNoMoreInteractions(client);
     });
   });
